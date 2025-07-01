2c0bc3df724b2b588f2ad3654b74090320d5fe0a
Sil0105 <pipita0105@gmail.com>
2025-07-01 20:13:21 -0300

    Agrego ejercicio008.js

---
+++ ejercicio008.js
@@ -0,0 +1,22 @@
+//a) Crear una función normalizarDatos() que utilice métodos de strings para:
+//✔ Convertir todos los títulos a mayúsculas.
+//✔ Eliminar espacios en blanco al inicio y final de los nombres de autores.
+//✔ Formatear los emails de los usuarios a minúsculas.
+
+function normalizarDatos() {
+    libros.forEach(libro => {
+        libro.titulo = libro.titulo.toUpperCase(); // Convertir títulos a mayúsculas con toUpperCase()
+    });
+
+    libros.forEach(libro => {
+        libro.autor = libro.autor.trim(); // String, elimina los espacios en blanco al inicio y final de los nombres de autores
+    });
+
+    usuarios.forEach(usuario => {
+        usuario.email = usuario.email.toLowerCase(); //Convierte todas las letras a minúsculas
+    });
+
+    console.log("Datos actualizados 📚👥:.");
+    console.log(libros);
+    console.log(usuarios);
+}
